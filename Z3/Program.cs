7ee8a35aee42b9eb0eda3733b2e2220643108db6
mkorobeynikov92 <kma992@mail.ru>
2023-11-22 15:37:46 +0300

    Выполнено третье задание

---
+++ Z3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число от 10 до 99 ");
+int number = int.Parse(Console.ReadLine()!);
+int firstnumber = number / 10;
+int secondnumber = number % 10;
+if (firstnumber > secondnumber)
+    Console.WriteLine(firstnumber);
+else
+    Console.WriteLine(secondnumber);
+
