8ab90615f2410acb363c999eb3244b37ca6cba11
mkorobeynikov92 <kma992@mail.ru>
2023-11-22 16:56:19 +0300

    сделали четвертое задание

---
+++ Z4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите число N ");
+int n = int.Parse(Console.ReadLine()!);
+if (n < 10)
+    Console.WriteLine(n);
+else
+while (n > 0)
+{
+int t = n % 10;
+n = n / 10;
+if(n > 0)
+Console.Write($"{t},");
+else
+Console.WriteLine(t);
+}
