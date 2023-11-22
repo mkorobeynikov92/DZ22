04c85d76c1484cd33b1c2322f6ca79869ffbdbe8
mkorobeynikov92 <kma992@mail.ru>
2023-11-22 14:48:55 +0300

    Выполнено задание 1

---
+++ Z1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number % 7 == 0 && number % 23 == 0)
+    Console.WriteLine("Кратно 7 и 23");
+else
+    Console.WriteLine("Не кратно");
